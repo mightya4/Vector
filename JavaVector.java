f43dc03084fad55f6eae7395a026973837d0bb02
Akeem <akeem.mighty@gmail.com>
2022-10-16 21:50:52 -0400

    Import Vector Class + Create Vector Object + Add Integer Elements to Vector + Output Each Element In Vector

--- JavaVector.java
+++ JavaVector.java
@@ -1,5 +1,18 @@
+import java.util.Vector;
+
 public class JavaVector {
     public static void main(String[] args) {
-        
+        //Instantiate Vector
+        Vector<Integer> v = new Vector<Integer>();
+
+        //Populate Vector
+        for(int i = 0; i <= 10; i++) {
+            v.add(i);
+        }
+
+        //Display Vector Elements
+        v.forEach((i) -> {
+            System.out.println(i);
+        });
     }
 }
